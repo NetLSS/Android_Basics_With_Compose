4fe38c069d9333cfd8e806d1d001db740ca40b4b
sslee <sangsulee@gmarket.com>
2022-05-03 16:21:38 +0900

    Feat: #1 Compose Tutorial

--- Composables_practice_problems/app/src/main/java/com/lilcode/test/composables_practice_problems/MainActivity.kt
+++ Composables_practice_problems/app/src/main/java/com/lilcode/test/composables_practice_problems/MainActivity.kt
@@ -3,13 +3,20 @@ package com.lilcode.test.composables_practice_problems
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.lilcode.test.composables_practice_problems.ui.theme.Composables_practice_problemsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Composables_practice_problemsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeTutorial() {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+
+        Text(
+            text = stringResource(R.string.title),
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+
+        Text(
+            text = stringResource(R.string.body1),
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+        Text(
+            text = stringResource(R.string.body2),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Composables_practice_problemsTheme {
-        Greeting("Android")
+        ComposeTutorial()
     }
 }
